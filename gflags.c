dac49f8dc4c2410af1220cd77d05b07e1088fcc0
olowoyobabajide <104766706+olowoyobabajide@users.noreply.github.com>
2023-06-30 23:20:46 +0100

    Update gflags_c

--- gflags.c
+++ gflags.c
@@ -2,23 +2,23 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @fmt: Formatted string
+ * @a: parameter
  * Return: Flags:
  */
-int get_flags(const char *format, int *i)
+int get_flags(const char *fmt, int *a)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
+	int j, curr_a;
+
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+	curr_a = *a + 1;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	while (fmt[curr_a] != '\0')
 	{
 		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
+			if (fmt[curr_a] == FLAGS_CH[j])
 			{
 				flags |= FLAGS_ARR[j];
 				break;
@@ -26,9 +26,9 @@ int get_flags(const char *format, int *i)
 
 		if (FLAGS_CH[j] == 0)
 			break;
+		curr_a++;
 	}
-
-	*i = curr_i - 1;
+	*a = curr_a - 1;
 
 	return (flags);
 }
