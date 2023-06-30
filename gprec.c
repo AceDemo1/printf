2af229914dac2cfd36db27e76c261aa5c373858f
olowoyobabajide <104766706+olowoyobabajide@users.noreply.github.com>
2023-06-30 23:25:46 +0100

    Update gprec_c

--- gprec.c
+++ gprec.c
@@ -1,41 +1,41 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_precision - Calculates the precision
+ * @fmt: Formatted string
+ * @a: args
+ * @arr: list of args.
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
+int get_precision(const char *fmt, int *a, va_list arr)
 {
-	int curr_i = *i + 1;
+	int curr_a = *a + 1;
 	int precision = -1;
 
-	if (format[curr_i] != '.')
+	if (fmt[curr_a] != '.')
 		return (precision);
 
 	precision = 0;
 
-	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_a += 1; fmt[curr_a] != '\0'; curr_a++)
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[curr_a]))
 		{
 			precision *= 10;
-			precision += format[curr_i] - '0';
+			precision += fmt[curr_a] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else if (fmt[curr_a] == '*')
 		{
-			curr_i++;
-			precision = va_arg(list, int);
+			curr_a++;
+			precision = va_arg(arr, int);
 			break;
 		}
 		else
 			break;
 	}
 
-	*i = curr_i - 1;
+	*a = curr_a - 1;
 
 	return (precision);
 }
