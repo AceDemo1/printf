c1ade9706bac32f0da2dd7f052aaa0b357501036
olowoyobabajide <104766706+olowoyobabajide@users.noreply.github.com>
2023-07-01 02:08:18 +0100

    Update printf

--- printf.c
+++ printf.c
@@ -1,43 +1,40 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
+ * _printf - Printf
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format, ...)
+int _printf(const char *fmt, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
+	va_list arr;
 	char buffer[BUFF_SIZE];
 
-	if (format == NULL)
+	if (fmt == NULL)
 		return (-1);
 
-	va_start(list, format);
+	va_start(arr, fmt);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	for (i = 0; fmt && fmt[i] != '\0'; i++)
 	{
-		if (format[i] != '%')
+		if (fmt[i] != '%')
 		{
-			buffer[buff_ind++] = format[i];
+			buffer[buff_ind++] = fmt[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			flags = get_flags(fmt, &i);
+			width = get_width(fmt, &i, arr);
+			precision = get_precision(fmt, &i, arr);
+			size = get_size(fmt, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = handle_print(fmt, &i, arr, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -45,19 +42,18 @@ int _printf(const char *format, ...)
 		}
 	}
 
-	print_buffer(buffer, &buff_ind);
+	print_buff(buffer, &buff_ind);
 
-	va_end(list);
+	va_end(arr);
 
 	return (printed_chars);
 }
-
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
  * @buff_ind: Index at which to add next char, represents the length.
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_buff(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
